f8f9e4021e7917d5f560bb6ed51abc51d8df0827
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-15 23:11:37 -0300

    bootstrap application

---
+++ app.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'bootstrap.php';
+
+$loader = require 'vendor/autoload.php';
+$loader->register();
+
+use Symfony\Component\HttpFoundation\Request;
+use App\Framework\Http\Kernel;
+
+$request = Request::createFromGlobals();
+$app = new Kernel();
+
+// API Routes.
+require 'app/Applications/Api/Http/routes.php';
+
+$response = $app->handle($request);
+
+if (! is_null($response)) {
+    $response->send();
+}
+
+
